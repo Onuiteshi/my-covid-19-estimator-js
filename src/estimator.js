815f6225e12fb08e6e52e0df186fd4d0da5dbc80
onuiteshi <emmanuelonuiteshi@gmail.com>
2020-04-12 11:29:18 +0100

    challenge 1 and 2 updated

--- src/estimator.js
+++ src/estimator.js
@@ -12,21 +12,40 @@ const covid19ImpactEstimator = () => {
     population: 66622705,
     totalHospitalBeds: 1380614
   };
+  let days;
+  if (input.periodType === 'days') {
+    days = input.timeToElapse;
+  } else if (input.periodType === 'weeks') {
+    days = input.timeToElapse * 7;
+  } else if (input.periodType === 'months') {
+    days = input.timeToElapse * 30;
+  }
+
+  const value = Math.trunc(days / 3);
+
   return {
     data: input,
     impact: {
       currentlyInfected: input.reportedCases * 10,
-      infectionsByRequestedTime: input.reportedCases * 10 * 512,
-      severeCasesByRequestedTime: 0.15 * (input.reportedCases * 10 * 512),
-      hospitalBedsByRequestedTime:
-        0.35 * input.totalHospitalBeds - 0.15 * (input.reportedCases * 10 * 512)
+      infectionsByRequestedTime: Math.trunc(input.reportedCases * 10 * value),
+      severeCasesByRequestedTime: Math.trunc(
+        0.15 * (input.reportedCases * 10 * value)
+      ),
+      hospitalBedsByRequestedTime: Math.trunc(
+        0.35 * input.totalHospitalBeds -
+          0.15 * (input.reportedCases * 10 * value)
+      )
     },
     severeImpact: {
       currentlyInfected: input.reportedCases * 50,
-      infectionsByRequestedTime: input.reportedCases * 50 * 512,
-      severeCasesByRequestedTime: 0.15 * (input.reportedCases * 50 * 512),
-      hospitalBedsByRequestedTime:
-        0.35 * input.totalHospitalBeds - 0.15 * (input.reportedCases * 50 * 512)
+      infectionsByRequestedTime: Math.trunc(input.reportedCases * 50 * value),
+      severeCasesByRequestedTime: Math.trunc(
+        0.15 * (input.reportedCases * 50 * value)
+      ),
+      hospitalBedsByRequestedTime: Math.trunc(
+        0.35 * input.totalHospitalBeds -
+          0.15 * (input.reportedCases * 50 * value)
+      )
     }
   };
 };
